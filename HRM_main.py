8ab469da99f303228a88430ef63073fd958ddc15
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-02-27 22:38:01 -0500

    restructure mechanism of importing files

--- HRM_main.py
+++ HRM_main.py
@@ -1,15 +1,22 @@
-class HeartRateMonitor:
+filename = 'test_data1.csv'
+def import_data():
+    import csv
+    with open(filename, 'r') as my_data:
+        csv_reader = csv.reader(my_data)
+
+        for reading in my_data:
+            time = reading[0]
+            voltage = reading[1]
+    return time, voltage
+
+
+class HeartRateData:
     def __init__(self, time, voltage):
         self.timevals = time
         self.voltagevals = voltage
 
-    def import_data():
-        import csv
-        with open('test_data1.csv','r') as my_data:
-            csv_reader = csv.reader(my_data)
+    def visualize(self):
+        import pandas
+        import matplot.pyplot as pl
 
-            for reading in my_data:
-                time = reading[0]
-                voltage = reading[1]
 
-HeartRateMonitor.import_data()
