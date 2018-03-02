80b52202a4c416f5903bcf6c90756ca7294f1997
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-03-01 19:31:58 -0500

    add capability to find interval between first two pulses

--- HRM_main.py
+++ HRM_main.py
@@ -1,4 +1,6 @@
-filename = '/Users/AnthonySchneider/Desktop/bme590hrm/test_data/test_data3.csv'
+
+
+filename = '/Users/AnthonySchneider/Desktop/bme590hrm/test_data/test_data12.csv'
 
 
 def import_csv_data(filename):
@@ -32,18 +34,39 @@ class HeartRateData:  # remember to have option to set units
         import matplotlib.pyplot as plt
         import peakutils
         import scipy.signal
-        t = 1
         x = self.voltagevals
         autocorr = (numpy.correlate(x, x, mode='full'))
-        autocorr_amplified = autocorr**2
-        half_corr = autocorr_amplified[autocorr_amplified.size // 2:]
-        plt.plot(half_corr)
+        autocorr_sqd = autocorr**2
+        autocorr_sqd = autocorr_sqd[len(autocorr_sqd)//2:]
+
+        return autocorr_sqd
+
+    def find_interval(self):
+        import numpy
+        import matplotlib.pyplot as plt
+        import peakutils
+        import scipy.signal
+        data = self.autocorrelate()
+        peaks_indices = scipy.signal.find_peaks_cwt(data,numpy.arange(5,10),min_snr=4)
+        max_values = []
+        for n,i in enumerate(peaks_indices):
+           max_values.append(data[i])
+        plt.plot(data)
+        plt.scatter(peaks_indices, max_values,marker='x', c='red')
         plt.show()
+        # Time to second peak in autocorr rep. one period
+        interval_time_index = peaks_indices[1]
+        interval = self.timevals[interval_time_index]
+        print(interval)
 
 
-Data1 = HeartRateData(time,voltage)
-Data1.autocorrelate()
+
+
+
+Data1 = HeartRateData(time, voltage)
 Data1.visualize()
+Data1.autocorrelate()
+Data1.find_interval()
 
 
 
