71ac7a49c116012d22cbbd73ebe68374892d6c10
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-03-01 21:46:31 -0500

    add ECG data peak detection capability

--- HRM_main.py
+++ HRM_main.py
@@ -1,6 +1,6 @@
 
 
-filename = '/Users/AnthonySchneider/Desktop/bme590hrm/test_data/test_data12.csv'
+filename = '/Users/AnthonySchneider/Desktop/bme590hrm/test_data/test_data5.csv'
 
 
 def import_csv_data(filename):
@@ -44,7 +44,6 @@ class HeartRateData:  # remember to have option to set units
     def find_interval(self):
         import numpy
         import matplotlib.pyplot as plt
-        import peakutils
         import scipy.signal
         data = self.autocorrelate()
         peaks_indices = scipy.signal.find_peaks_cwt(data,numpy.arange(5,10),min_snr=4)
@@ -57,7 +56,44 @@ class HeartRateData:  # remember to have option to set units
         # Time to second peak in autocorr rep. one period
         interval_time_index = peaks_indices[1]
         interval = self.timevals[interval_time_index]
-        print(interval)
+        return interval
+
+    def count_beats(self):
+        import matplotlib.pyplot as plt
+
+        interval_sec = self.find_interval()
+        interval_indices = self.timevals.index(interval_sec)
+        num_intervals = int(max(self.timevals)/interval_sec)
+
+        # Create interval "search bins" to find peaks
+        bin_ends = []
+        for i in range(1,num_intervals+1):
+            bin_ends.append((i*interval_indices)-1)
+
+        start = 0
+        peak_val = []
+        peak_val_index= []
+
+        for n,i in enumerate(bin_ends):
+            bin = self.voltagevals[start:i]
+            peak_val.append(max(bin))
+            peak_val_location = start + bin.index(peak_val[n])
+            peak_val_index.append(peak_val_location)
+            start = i+1
+
+        peak_val_times = []
+        for i in peak_val_index:
+            peak_val_times.append(self.timevals[i])
+
+        # Graph each "search bin" and mark maxima
+        for i in range(1,num_intervals+1):
+            plt.axvline(i*interval_sec,c='red')
+        plt.plot(self.timevals, self.voltagevals)
+        plt.scatter(peak_val_times, peak_val, marker='x', c='red')
+        plt.show()
+
+
+
 
 
 
@@ -65,8 +101,7 @@ class HeartRateData:  # remember to have option to set units
 
 Data1 = HeartRateData(time, voltage)
 Data1.visualize()
-Data1.autocorrelate()
-Data1.find_interval()
+Data1.count_beats()
 
 
 
