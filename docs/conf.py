7016700e383cb3bc1d5d8d209c0d8cc0f0577813
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-03-02 21:38:39 -0500

    correct pep8 in conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/Users/AnthonySchneider/Desktop/bme590hrm'))
+sys.path.insert(0, os.path.abspath('/Users/AnthonySchneider/Desktop/bme590hrm'
+                                   ))
 
 
 # -- Project information -----------------------------------------------------
